c0c04593012ddd9792ade9fb89628addf866600d
MakotoKurauchi <pluis@me.com>
2022-11-03 11:07:30 +0900

    change default keymap

--- keyboards/yushakobo/quick_paint/keymaps/default/keymap.c
+++ keyboards/yushakobo/quick_paint/keymaps/default/keymap.c
@@ -4,7 +4,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
             LCTL(KC_Z), LCTL(KC_Y), KC_G, KC_P, KC_LALT,
             KC_M, LCTL(KC_SPC), KC_E, KC_B, KC_RBRC,
-            KC_W, LSFT(KC_SPC), KC_K, KC_SPC, KC_LBRC
+            KC_W, LSFT(KC_SPC), KC_K, KC_SPC, KC_BSLS
         )
 };
 
