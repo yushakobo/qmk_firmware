fdf8e5294bc37bb561d501149dd9a23cf1a60a31
Marby3 <murasugi@yushakobo.jp>
2025-07-10 16:44:34 +0900

    YK60 firmware

---
+++ keyboards/yushakobo/yk60/keymaps/default/config.h
@@ -0,0 +1,8 @@
+// Copyright 2025 yushakobo
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* Reset */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
