cfd5effe314bebe9aa9317d51fbb448ed58eee70
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2026-05-14 17:42:01 +0900

    add: new keyboard definition for NekoPad

---
+++ keyboards/yushakobo/nekopad/keymaps/default/keymap.c
@@ -0,0 +1,17 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_2x2(
+        KC_1,                      KC_2,
+                 KC_3,    KC_4
+    )
+};
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) }
+};
+#endif
