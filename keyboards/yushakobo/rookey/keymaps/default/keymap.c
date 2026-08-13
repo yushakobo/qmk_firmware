3f449bcb1a1511cae92799f2144f05e32a8d087d
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2026-08-13 16:25:13 +0900

    add: our variant of Rookey keyboard

---
+++ keyboards/yushakobo/rookey/keymaps/default/keymap.c
@@ -0,0 +1,18 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R,
+        KC_A, KC_S, KC_D, KC_F,
+        KC_V
+    )
+};
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+  [0] = { ENCODER_CCW_CW(KC_L, KC_R) }
+};
+#endif
